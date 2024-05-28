2accfcdd24165ae481b03bc561a1b21809983b46
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-05-28 12:14:43 -0300

    add example of evm storage for mapping of dynamic array

--- Mapping.sol
+++ Mapping.sol
@@ -6,26 +6,31 @@ contract Mapping {
 
     mapping (address => uint256) public map;
     mapping (address => mapping(address => uint256)) public map2;
-
+    mapping (address => uint256[]) private map3;
 
     address public constant ADDR_1 = address(1);
     address public constant ADDR_2 = address(2);
     address public constant ADDR_3 = address(3);
 
     constructor(){
-        map[ADDR_1] = 100;
-        map[ADDR_1] = 200;
-        map[ADDR_1] = 300;
+        map[ADDR_1] = 10;
+        map[ADDR_2] = 20;
+        map[ADDR_3] = 30;
 
         // map2[key0, key1]
         map2[ADDR_1][ADDR_2] = 100;
         map2[ADDR_2][ADDR_3] = 200;
         map2[ADDR_3][ADDR_1] = 300;
+
+        map3[ADDR_1].push(1000);
+        map3[ADDR_2].push(2000);
+        map3[ADDR_3].push(3000);
     }
 
-    function get (address _key) public view returns (uint256 value){
-        assembly{
-            value := sload(keccak256(abi.encode(_key, uint256(0))))
+    function getSimple (address _key) public view returns (uint256 value){
+        bytes32 slotKey = keccak256(abi.encode(_key, uint256(0)));
+        assembly {
+            value := sload(slotKey)
         }
     }    
 
@@ -33,6 +38,15 @@ contract Mapping {
         bytes32 slotKey = keccak256(abi.encode(_key1, keccak256(abi.encode(_keyO, uint256(1)))));
         assembly {
             value := sload(slotKey)
+        }
+    }
+
+    function getMappingArray (address _key, uint256 _i) public view returns (uint256 length, uint256 value) {
+        bytes32 mapElementKey = keccak256(abi.encode(_key, uint256(2)));
+        bytes32 finalElementKey = keccak256(abi.encode(mapElementKey));
+        assembly {
+            length := sload(mapElementKey)
+            value := sload(add(finalElementKey, _i))
         }
     }
 }
