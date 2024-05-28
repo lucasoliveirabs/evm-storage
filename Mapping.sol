06096e84e37b98ee5d4d2b5e1a8e91cdac36f39d
lucasoliveirabs <lucasoliveirabs95@gmail.com>
2024-05-28 09:04:13 -0300

    add example of evm storage of nested mapping

--- Mapping.sol
+++ Mapping.sol
@@ -5,6 +5,8 @@ contract Mapping {
     // slotKey = keccak256(key, base_slot key)
 
     mapping (address => uint256) public map;
+    mapping (address => mapping(address => uint256)) public map2;
+
 
     address public constant ADDR_1 = address(1);
     address public constant ADDR_2 = address(2);
@@ -14,6 +16,11 @@ contract Mapping {
         map[ADDR_1] = 100;
         map[ADDR_1] = 200;
         map[ADDR_1] = 300;
+
+        // map2[key0, key1]
+        map2[ADDR_1][ADDR_2] = 100;
+        map2[ADDR_2][ADDR_3] = 200;
+        map2[ADDR_3][ADDR_1] = 300;
     }
 
     function get (address _key) public view returns (uint256 value){
@@ -21,4 +28,11 @@ contract Mapping {
             value := sload(keccak256(abi.encode(_key, uint256(0))))
         }
     }    
+
+    function getNested (address _keyO, address _key1) public view returns (uint256 value) {
+        bytes32 slotKey = keccak256(abi.encode(_key1, keccak256(abi.encode(_keyO, uint256(1)))));
+        assembly {
+            value := sload(slotKey)
+        }
+    }
 }
